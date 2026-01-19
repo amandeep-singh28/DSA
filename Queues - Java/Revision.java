ec0809450ab5449e8df0afc59dabe821b44d274e
Amandeep Singh <amandeep87557@gmail.com>
2026-01-19 10:18:53 +0530

    Linked List Implementation Queue

--- Queues - Java/Revision.java
+++ Queues - Java/Revision.java
@@ -26,7 +26,7 @@ import java.util.Queue;
     }    
 }*/
 
-public class Revision {
+/* public class Revision {
     public static class queueArray {
         int front = -1, rear = -1, size = 0;
         int array[] = new int[100];
@@ -84,5 +84,68 @@ public class Revision {
         q.remove();
         q.display();
         q.peek();
+    }
+}*/
+
+public class Revision {
+    public static class Node {
+        int data;
+        Node next;
+        Node (int data) {
+            this.data = data;
+        }
+    }
+    public static class linkedlistq1 {
+        Node head, tail = null;
+        int size = 0;
+        public void push(int x) {
+            Node temp = new Node(x);
+            if (head == null) {
+                head = tail = temp;
+            } else {
+                tail.next = temp;
+                tail = temp;
+            }
+            size++;
+        }
+
+        public void remove() {
+            if (size == 0) {
+                System.out.println("Queue is empty!!");
+                return;
+            }
+            System.out.println("Element removed:" + head.data);
+            head = head.next;
+        }
+
+        public void peek() {
+            if (size == 0) {
+                System.out.println("Queue is empty!!");
+                return;
+            }
+            System.out.println("Top Element:" + head.data);
+        }
+
+        public void display() {
+            Node temp = head;
+            if (size == 0) {
+                System.out.println("Queue is empty!!");
+                return;
+            }
+            while (temp != null) {
+                System.out.println(temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        linkedlistq1 q1 = new linkedlistq1();
+        q1.push(10);
+        q1.push(20);
+        q1.push(30);
+        q1.display();
+        q1.remove();
+        q1.display();
+        q1.peek();
     }
 }
