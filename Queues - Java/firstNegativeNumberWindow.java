63669f3704d3f0df78df6507f31a11f2ca7942e6
Amandeep Singh <amandeep87557@gmail.com>
2026-01-20 20:43:24 +0530

    Optimized First Negative Number Window O(n)

--- Queues - Java/firstNegativeNumberWindow.java
+++ Queues - Java/firstNegativeNumberWindow.java
@@ -1,27 +1,62 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class firstNegativeNumberWindow {
     public static void main(String[] args) {
+        // int arr[] = {12, -1, -7, 8, -15, 30, 16, 28};
+        // int k = 3;
+        // int size = arr.length;
+        // int res[] = new int[size - k + 1];
+
+        // for (int i = 0; i < size - k + 1; i++) {
+        //     boolean found = false;
+        //     for (int j = 0; j < k; j++) {
+        //         if (arr[i + j] < 0) {
+        //             res[i] = arr[i + j];
+        //             found = true;
+        //             break;
+        //         }
+        //     }
+        //     if (found == false) {
+        //         res[i] = 0;
+        //     }
+        // }
+
+        // for (int i = 0; i < res.length; i++) {
+        //     System.out.print(res[i] + " ");
+        // }
+
         int arr[] = {12, -1, -7, 8, -15, 30, 16, 28};
         int k = 3;
-        int size = arr.length;
-        int res[] = new int[size - k + 1];
+            int size = arr.length;
+            int res[] = new int[size - k + 1];
 
-        for (int i = 0; i < size - k + 1; i++) {
-            boolean found = false;
-            for (int j = 0; j < k; j++) {
-                if (arr[i + j] < 0) {
-                    res[i] = arr[i + j];
-                    found = true;
-                    break;
+            Queue<Integer> q1 = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                if (arr[i] < 0) {
+                    q1.add(i);
                 }
             }
-            if (found == false) {
-                res[i] = 0;
-            }
-        }
+            System.out.println(q1);
 
-        for (int i = 0; i < res.length; i++) {
-            System.out.print(res[i] + " ");
-        }
+            for (int i = 0; i < size - k + 1; i++) {
+                if (q1.size() > 0 && q1.peek() < i) {
+                    q1.remove();
+                }
+                if (q1.size() > 0 && q1.peek() <= i + k - 1) {
+                    res[i] = arr[q1.peek()];
+                }
+                else if (q1.size() == 0) {
+                    res[i] = 0;
+                }
+                else {
+                    res[i] = 0;
+                }
+            }
+            
+            for (int i = 0; i < res.length; i++) {
+                System.out.print(res[i] + " ");
+            }
 
     }
 }
