97d59fe81b6464df0955f1784a7227c2bdd382e7
Amandeep Singh <amandeep87557@gmail.com>
2026-01-21 19:10:21 +0530

    Max Priority Queue

--- Java Collection Framework/frameworkPriorityQueue.java
+++ Java Collection Framework/frameworkPriorityQueue.java
@@ -1,5 +1,6 @@
 // Processing is based upon priority
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class frameworkPriorityQueue {
@@ -12,9 +13,22 @@ public class frameworkPriorityQueue {
         System.out.println(pq); // Gurantee - topmost priority element will be processed first
         System.out.println(pq.poll()); // 5
         System.out.println(pq); // Now the topmost element will be 7
+        
+    }
+    
+    static void MaxPriorityQueueExamples() {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder()); // Max PriorityQueue
+        pq.add(10);
+        pq.add(5);
+        pq.add(7);
+        System.out.println(pq.peek()); // 10
+        System.out.println(pq);  // 10 5 7
+        System.out.println(pq.poll()); // 10 
+        System.out.println(pq); // 7 5
 
     }
     public static void main(String[] args) {
         PriorityQueueExamples();
+        MaxPriorityQueueExamples();
     }
 }
