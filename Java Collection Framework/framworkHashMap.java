b972f0987b5534ec755c84d90acc2f752e412752
Amandeep Singh <amandeep87557@gmail.com>
2026-01-21 20:08:31 +0530

    HashMap Iteration

--- Java Collection Framework/framworkHashMap.java
+++ Java Collection Framework/framworkHashMap.java
@@ -6,14 +6,31 @@ public class framworkHashMap {
         mp.put(3, "Aman");
         mp.put(1, "Ravi");
         mp.put(2, "Riya");
-        mp.put(3, "Amandeep"); // It will update
-        mp.putIfAbsent(1, "Amandeep Singh");
-        System.out.println(mp);
-        System.out.println(mp.get(2)); // Riya
-        System.out.println(mp.containsKey(4)); // false
-        System.out.println(mp.containsValue("Aman")); // true
-        System.out.println(mp.entrySet());
-        System.out.println(mp.keySet());
-        System.out.println(mp.values());
+
+        //Iterating over keys in a map
+        for (Integer i : mp.keySet()){
+            System.out.println(i);
+        }
+
+        //Iterating over values in a map
+        for (var i : mp.values()) { // If we will use var than we don't need to specify any data types
+            System.out.println(i);
+        }
+
+        //Iterate over the key, value mapping
+        for (var e : mp.entrySet()) {
+            System.out.println(e.getKey());
+            System.out.println(e.getValue());
+        }
+
+        // mp.put(3, "Amandeep"); // It will update
+        // mp.putIfAbsent(1, "Amandeep Singh");
+        // System.out.println(mp);
+        // System.out.println(mp.get(2)); // Riya
+        // System.out.println(mp.containsKey(4)); // false
+        // System.out.println(mp.containsValue("Aman")); // true
+        // System.out.println(mp.entrySet());
+        // System.out.println(mp.keySet());
+        // System.out.println(mp.values());
     }
 }
