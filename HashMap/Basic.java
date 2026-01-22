ace1f7a3935a2f45f16e2bf4bc446e772328a7e4
Amandeep Singh <amandeep87557@gmail.com>
2026-01-22 13:12:24 +0530

    Java HashMap Functions and Implementation

---
+++ HashMap/Basic.java
@@ -0,0 +1,49 @@
+import java.util.HashMap;
+
+public class Basic {
+    public static void main(String[] args) {
+        // Syntax
+        HashMap<String, Integer> mp = new HashMap<>();
+        // Adding Elements
+        mp.put("Akash", 21);
+        mp.put("Yash", 16);
+        mp.put("Nashit", 17);
+        mp.put("Rishika", 19);
+        mp.put("Harry", 18);
+        //Getting value of a key from the HashMap
+        System.out.println(mp.get("Akash"));
+        System.out.println(mp.get("Rahul")); // It will return Null
+        //Changing/updating value of a key in the HashMap
+        mp.put("Akash", 25);
+        System.out.println(mp.get("Akash"));
+        //Removing a pair from the HashMap
+        System.out.println(mp.remove("Akash"));
+        System.out.println(mp.remove("Aman")); // null
+        //Checking if a key is in the HashMap
+        System.out.println(mp.containsKey("Akash"));
+        System.out.println(mp.containsKey("Nashit"));
+        //Adding a new entry only if the new key doesn't exist already
+        mp.putIfAbsent("Yashika", 30); // will enter
+        mp.putIfAbsent("Yash", 30); // will not enter
+        //Get all keys in the HashMap
+        System.out.println(mp.keySet());
+        //Get all values in the HashMap
+        System.out.println(mp.values());
+        //Get all entries in the HashMap
+        System.out.println(mp.entrySet()); 
+        //Traversing all entries of HashMap - multiple methods
+        for (String key : mp.keySet()) {
+            System.out.printf("Age of %s is %d\n", key, mp.get(key));
+        }
+        for (Integer value : mp.values()) {
+            System.out.println(value);
+        }
+        for (HashMap.Entry<String, Integer> e : mp.entrySet()) {
+            System.out.printf("Age of %s is %d\n", e.getKey(), e.getValue());
+        }
+        // OR
+        for (var e : mp.entrySet()) {
+            System.out.printf("Age of %s is %d\n", e.getKey(), e.getValue());
+        }
+    }
+}
