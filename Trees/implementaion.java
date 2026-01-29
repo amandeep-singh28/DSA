9c71202681a9d843a441ca3f62054884b5104045
Amandeep Singh <amandeep87557@gmail.com>
2026-01-29 12:53:21 +0530

    Displaying a Tree

--- Trees/implementaion.java
+++ Trees/implementaion.java
@@ -10,6 +10,15 @@ public class implementaion {
             this.val = val;
         }
     }
+    public static void display(Node root) {
+        if (root == null) return;
+        System.out.print(root.val + " -> ");
+        if (root.left != null) System.out.print(root.left.val + " ");
+        if (root.right != null) System.out.print(root.right.val);
+        System.out.println();
+        display(root.left);
+        display(root.right);
+    }
     public static void main(String[] args) {
         // Node root = new Node();
         // root.val = 10;
@@ -18,22 +27,23 @@ public class implementaion {
         // System.out.println(root.val);
 
         // Through Constructor
-        Node root = new Node(1);
-        Node a = new Node(2);
-        Node b = new Node(3);
+        Node root = new Node(2);
+        Node a = new Node(4);
+        Node b = new Node(10);
 
         root.left = a;
         root.right = b;
 
-        Node c = new Node(4);
+        Node c = new Node(6);
         Node d = new Node(5);
 
         a.left = c;
         a.right = d;
 
-        Node e = new Node(6);
+        Node e = new Node(11);
         b.right = e;
 
-        System.out.println(root.val);
+        // System.out.println(root.val);
+        display(root);
     }
 }
