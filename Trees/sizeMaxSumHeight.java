860bb02261921e719be3801798c7fd1d61647905
Amandeep Singh <amandeep87557@gmail.com>
2026-01-30 19:02:45 +0530

    Find Size and Sum of the binary tree

---
+++ Trees/sizeMaxSumHeight.java
@@ -0,0 +1,42 @@
+package Trees;
+
+public class sizeMaxSumHeight {
+    public static class Node {
+        int val; 
+        Node left;
+        Node right;
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+
+    public static int sum(Node root) {
+        if (root == null) return 0;
+        return root.val + sum(root.left) + sum(root.right);
+    }
+
+    public static int size(Node root) {
+        if (root == null) return 0;
+        return 1 + size(root.left) + size(root.right);
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        Node a = new Node(2);
+        Node b = new Node(3);
+
+        root.left = a;
+        root.right = b;
+
+        Node c = new Node(4);
+        Node d = new Node(5);
+
+        a.left = c;
+        a.right = d;
+
+        Node e = new Node(6);
+        b.right = e;
+        System.out.println(size(root));
+        System.out.println(sum(root));
+    }
+}
