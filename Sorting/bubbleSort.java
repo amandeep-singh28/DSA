c74827e10fbf1a2a19f3b5a0c4e8f7b96df3e8a3
Amandeep Singh <amandeep87557@gmail.com>
2026-02-07 18:54:58 +0530

    Bubble Sort

---
+++ Sorting/bubbleSort.java
@@ -0,0 +1,26 @@
+package Sorting;
+
+public class bubbleSort {
+    public static void print(int[] arr) {
+        for (int element : arr) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {5, -2, 6, 7, 2, 0, 7, 2};
+        int length = arr.length;
+        print(arr);
+        
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = 0; j < length - 1 - i; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        print(arr);
+    }
+}
