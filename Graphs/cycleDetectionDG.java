e3a1b77fdb3d3ea924be2ed0d6c342f5d1ff3821
Amandeep Singh <amandeep87557@gmail.com>
2026-02-11 10:12:46 +0530

    Cycle Detection in directed graph using Kahn's Algorithm(BFS)

---
+++ Graphs/cycleDetectionDG.java
@@ -0,0 +1,50 @@
+package Graphs;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class cycleDetectionDG {
+    public boolean canFinish(int n, int[][] edges) {
+        int indegree[] = new int[n];
+        boolean vis[] = new boolean[n];
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            List<Integer> list = new ArrayList<>();
+            adj.add(list);
+        }
+        for (int i = 0; i < edges.length; i++) {
+            int a = edges[i][0], b = edges[i][1]; // b -> a
+            adj.get(b).add(a);
+            indegree[a]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i < vis.length; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+                vis[i] = true;
+            }
+        }
+        while (q.size() > 0) {
+            int front = q.remove();
+            ans.add(front);
+            for (int element : adj.get(front)) {
+                indegree[element]--;
+                if (indegree[element] == 0) {
+                    q.add(element);
+                    vis[element] = true;
+                }
+            }
+        }
+        if (ans.size() == n)
+            return true;
+        else
+            return false;
+
+    }
+}
