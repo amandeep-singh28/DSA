172f597f6fd4f4e80fe5e62ebfef99e48947eeb4
Amandeep Singh <amandeep87557@gmail.com>
2026-02-19 20:59:08 +0530

    Network Delay Time(Bellman Ford Algo + Negative cycle)

--- Graphs/bellmanFordAlgo.java
+++ Graphs/bellmanFordAlgo.java
@@ -9,8 +9,8 @@ public class bellmanFordAlgo {
         Arrays.fill(ans, Integer.MAX_VALUE);
         ans[src] = 0;
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < times.length; j++) {
+        for (int i = 1; i <= n; i++) { // n - 1 edges travel
+            for (int j = 0; j < times.length; j++) { // All edges relaxed
                 int u = times[j][0];
                 int v = times[j][1];
                 int wt = times[j][2];
@@ -20,6 +20,18 @@ public class bellmanFordAlgo {
 
             }
         }
+
+        // nth time relaxation for -ve detection
+        for (int j = 0; j < times.length; j++) { // All edges relaxed
+                int u = times[j][0];
+                int v = times[j][1];
+                int wt = times[j][2];
+                if (ans[u] != Integer.MAX_VALUE && ans[u] + wt < ans[v]) {
+                    return -1; // Indication -ve cycle
+                }
+
+            }
+
         int max = -1;
         for (int i = 1; i <= n; i++) {
             if (ans[i] == Integer.MAX_VALUE) return -1;
