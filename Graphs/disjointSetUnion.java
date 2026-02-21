8264b64e67b4f4b1dcead05fb5f831ad929b98b5
Amandeep Singh <amandeep87557@gmail.com>
2026-02-21 19:54:32 +0530

    Number of provinces(Disjoint Set Union)

---
+++ Graphs/disjointSetUnion.java
@@ -0,0 +1,37 @@
+package Graphs;
+
+public class disjointSetUnion {
+    public static int find(int a, int[] parent) {
+        if (parent[a] == a) return a;
+        return find(parent[a], parent);
+    }
+    public static void union(int a, int b, int[] parent) {
+        int leaderA = find(a, parent);
+        int leaderB = find(b, parent);
+        if (leaderA != leaderB) {
+            parent[leaderB] = leaderA;
+        }
+    }
+    public static int findCircleNum(int[][] adj) {
+        int n = adj.length;
+        int[] parent = new int[n + 1]; // 1 to n nodes
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (adj[i][j] == 1) {
+                    union(i + 1, j + 1, parent);
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (parent[i] == i) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
