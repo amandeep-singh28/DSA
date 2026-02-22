94f6f568b548ce6b3ee07cabd6ac0535fb72654a
Amandeep Singh <amandeep87557@gmail.com>
2026-02-22 18:36:07 +0530

    Cycle detection using DSU

---
+++ Graphs/cycleDetectionDSU.java
@@ -0,0 +1,37 @@
+package Graphs;
+
+public class cycleDetectionDSU {
+    public int find(int a, int[] parent) {
+        if (parent[a] == a) return a;
+        return find(parent[a], parent);
+    }
+    public void union(int a, int b, int[] parent) {
+        int leaderA = find(a, parent);
+        int leaderB = find(b, parent);
+        if (leaderA != leaderB) {
+            parent[leaderA] = parent[leaderB];
+        }
+    }
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        int m = edges[0].length;
+
+        int ans[] = new int[2];
+        int parent[] = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+        for (int i = 0; i < n; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            if (find(u, parent) == find(v, parent)) {
+                ans[0] = u;
+                ans[1] = v;
+                break;
+            } else {
+                union(u, v, parent);
+            }
+        }
+        return ans;
+    }
+}
