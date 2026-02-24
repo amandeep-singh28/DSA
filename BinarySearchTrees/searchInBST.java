d5a81ab12ee9b3979564af009739ebc1f3dd36b4
Amandeep Singh <amandeep87557@gmail.com>
2026-02-24 18:32:00 +0530

    Search in BST

---
+++ BinarySearchTrees/searchInBST.java
@@ -0,0 +1,32 @@
+package BinarySearchTrees;
+
+public class searchInBST {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        } else if (val > root.val) {
+            return searchBST(root.right, val);
+        } else {
+            return searchBST(root.left, val);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
