cd3bea22f08ae6d9a7833a2503f111046707c6fc
Amandeep Singh <amandeep87557@gmail.com>
2026-02-24 18:48:47 +0530

    Insertion BST

---
+++ BinarySearchTrees/insertBST.java
@@ -0,0 +1,31 @@
+package BinarySearchTrees;
+
+public class insertBST {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+        
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) return new TreeNode(val);
+        if (val > root.val) {
+            if (root.right == null) root.right = new TreeNode(val);
+            else insertIntoBST(root.right, val);
+        } else if (val < root.val) {
+            if (root.left == null) root.left = new TreeNode(val);
+            else insertIntoBST(root.left, val);
+        }
+        return root;
+    }
+}
