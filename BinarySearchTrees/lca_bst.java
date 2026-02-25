7bce9beb7d83eae9c902eb54b87da9cf1d1ceb0d
Amandeep Singh <amandeep87557@gmail.com>
2026-02-25 20:04:25 +0530

    Lowest common ancestor BST

---
+++ BinarySearchTrees/lca_bst.java
@@ -0,0 +1,19 @@
+package BinarySearchTrees;
+
+public class lca_bst {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+}
+ 
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val == root.val || q.val == root.val) return root;
+        if (p.val > root.val && q.val < root.val) return root;
+        if (q.val > root.val && p.val < root.val) return root;
+        if (p.val > root.val && q.val > root.val) return lowestCommonAncestor(root.right, p, q);
+        if (p.val < root.val && q.val < root.val) return lowestCommonAncestor(root.left, p, q);
+        return null;
+    }
+}
