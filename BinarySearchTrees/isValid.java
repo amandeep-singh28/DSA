31f0a0679370a77f942d1de5f5fc9d1f46d807bf
Amandeep Singh <amandeep87557@gmail.com>
2026-02-27 17:58:24 +0530

    Valid BST or not

---
+++ BinarySearchTrees/isValid.java
@@ -0,0 +1,29 @@
+package BinarySearchTrees;
+
+public class isValid {
+    public class TreeNode {
+        TreeNode left;
+        TreeNode right;
+        int val;
+
+    }
+    static boolean flag = true;
+    static TreeNode prev = null;
+    public void inorder(TreeNode root) {
+        if (root == null) return;
+        inorder(root.left);
+        if (prev == null) prev = root;
+        else if (root.val <= prev.val) {
+            flag = false;
+        } else {
+            prev = root;
+        }
+        inorder(root.right);
+    }
+    public boolean isValidBST(TreeNode root) {
+        flag = true;
+        prev = null;
+        inorder(root);
+        return flag;
+    }
+}
