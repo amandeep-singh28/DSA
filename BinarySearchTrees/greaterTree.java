5c1dd74519ec9058e742610c96e02eee2702f767
Amandeep Singh <amandeep87557@gmail.com>
2026-02-27 20:06:40 +0530

    Convert BST to Greater Tree(By locally -> Using int[])

--- BinarySearchTrees/greaterTree.java
+++ BinarySearchTrees/greaterTree.java
@@ -10,17 +10,18 @@ public class greaterTree {
         }
 
     }
-    static int sum = 0;
-    public void reverseInorderTraversal(TreeNode root) {
+    // static int sum = 0;
+    public void reverseInorderTraversal(TreeNode root, int[] sum) {
         if (root == null) return;
-        reverseInorderTraversal(root.right);
-        root.val = root.val + sum;
-        sum = root.val;
-        reverseInorderTraversal(root.left);
+        reverseInorderTraversal(root.right, sum);
+        root.val = root.val + sum[0];
+        sum[0] = root.val;
+        reverseInorderTraversal(root.left, sum);
     }
     public TreeNode bstToGst(TreeNode root) {
-        sum = 0;
-        reverseInorderTraversal(root);
+        int[] sum = new int[1];
+        // sum = 0;
+        reverseInorderTraversal(root, sum);
         return root;
     }
 }
