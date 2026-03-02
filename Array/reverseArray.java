ab6c65c0f16785a608e1c230e883d5f980424a60
Amandeep Singh <amandeep87557@gmail.com>
2026-03-02 19:04:16 +0530

    Inplace reverse array

---
+++ Array/reverseArray.java
@@ -0,0 +1,17 @@
+public class reverseArray {
+    static void reverse(int arr[]) {
+        int len = arr.length;
+        for (int i = 0; i < len / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[len - 1 - i];
+            arr[len - 1 - i] = temp;
+        }
+        for (int i = 0; i < len; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5, 6};
+        reverse(arr);
+    }
+}
