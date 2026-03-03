ff8840518c6b5ba3d8e1be1c0ff641d62b8f2ffd
Amandeep Singh <amandeep87557@gmail.com>
2026-03-03 12:46:44 +0530

    Sort array(left -> even && right -> odd)

---
+++ Array/sortArrayByParity.java
@@ -0,0 +1,20 @@
+public class sortArrayByParity {
+    public int[] sortArray(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            if (arr[left] % 2 == 0) {
+                left++;
+            } else if (arr[right] % 2 != 0) {
+                right--;
+            } else {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        return arr;
+    }
+}
