5ae54d66d9e72bec4031cda5d0366085ffe60a45
Amandeep Singh <amandeep87557@gmail.com>
2026-03-03 12:40:29 +0530

    Sort 0s and 1s using two pointer

--- Array/sort0sAnd1s.java
+++ Array/sort0sAnd1s.java
@@ -15,8 +15,31 @@ public class sort0sAnd1s {
             System.out.print(element + " ");
         }
     }
+    static void twoPointer(int arr[]) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            if (arr[left] == 0) {
+                left++;
+            } else if (arr[right] == 1) {
+                right--;
+            } else {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+            
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
     public static void main(String[] args) {
         int arr[] = {1, 0, 0, 1, 0, 1, 1, 0, 0};
-        sort(arr);
+        // sort(arr);
+        twoPointer(arr);
     }
 }
