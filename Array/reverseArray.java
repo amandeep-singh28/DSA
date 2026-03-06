6e258e37ce90d9bdc35cda5117dd643584d50b7a
Amandeep Singh <amandeep87557@gmail.com>
2026-03-06 10:36:23 +0530

    Array Reverse(While Loop)

--- Array/reverseArray.java
+++ Array/reverseArray.java
@@ -10,8 +10,23 @@ public class reverseArray {
             System.out.print(arr[i] + " ");
         }
     }
+    static void whileReverse(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
     public static void main(String[] args) {
         int arr[] = {1, 2, 3, 4, 5, 6};
-        reverse(arr);
+        // reverse(arr);
+        whileReverse(arr);
     }
 }
