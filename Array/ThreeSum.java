2f06e3f502bedc520d27668c9b43954f7dc206b2
Amandeep Singh <amandeep87557@gmail.com>
2026-03-08 11:57:01 +0530

    Three Sum using ArrayList and Set

---
+++ Array/ThreeSum.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ThreeSum {
+    class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        // List<List<Integer>> list = new ArrayList<>();
+        Set<List<Integer>> set = new HashSet<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        set.add(temp);
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(set);
+    }
+}
+}
