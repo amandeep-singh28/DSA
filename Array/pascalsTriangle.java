0a160b5ac70e0288606d7a2da30c0624a8c76e66
Amandeep Singh <amandeep87557@gmail.com>
2026-03-08 10:10:51 +0530

    Pascals Triangle

---
+++ Array/pascalsTriangle.java
@@ -0,0 +1,22 @@
+public class pascalsTriangle {
+    public static void main(String[] args) {
+        int n = 5;
+        int arr[][] = new int[n][];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = new int[i + 1];
+            arr[i][0] = 1;
+            arr[i][i] = 1;
+            for (int j = 1; j < i; j++) {
+                arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
