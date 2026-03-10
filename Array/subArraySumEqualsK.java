0c1cc7a4453c9dd682cca77376804ac6d6b8680c
Amandeep Singh <amandeep87557@gmail.com>
2026-03-10 12:34:17 +0530

    SubArray sums equals k(O(n^2))

---
+++ Array/subArraySumEqualsK.java
@@ -0,0 +1,13 @@
+public class subArraySumEqualsK {
+    public int subarraySum(int[] nums, int target) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum +=  nums[j];
+                if (sum == target) count++;
+            }
+        }
+        return count;
+    }
+}
