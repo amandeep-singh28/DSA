4e92b7d415b30d4aa580e557881afb5aa284757a
Amandeep Singh <amandeep87557@gmail.com>
2026-03-10 18:56:15 +0530

    Substring(i,j) && Substring(i)

---
+++ Strings/substr.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class substr {
+    public static void main(String[] args) {
+        String str = "Amandeep";
+        System.out.println(str.substring(0, 8));
+        System.out.println(str.substring(0));
+    }
+}
