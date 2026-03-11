2c6dda3b7c01394a51e1a9a00e0b701fa5e23dca
Amandeep Singh <amandeep87557@gmail.com>
2026-03-11 18:47:27 +0530

    Toggle Character with the help of StringBuilder

---
+++ Strings/toggleCharacters.java
@@ -0,0 +1,22 @@
+package Strings;
+import java.util.Scanner;
+
+public class toggleCharacters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder str = new StringBuilder(sc.nextLine());
+        System.out.println(str);
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'A' && ch <= 'Z') {
+                int ele = ch + 32;
+                str.setCharAt(i, (char)ele);
+            } else if (ch >= 'a' && ch <= 'z'){
+                int ele = ch - 32;
+                str.setCharAt(i, (char)ele);
+            }
+        }
+        System.out.println(str);
+        sc.close();
+    }
+}
