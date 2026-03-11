07751a71c81fd746ab074b33ceda6c984cf43272
Amandeep Singh <amandeep87557@gmail.com>
2026-03-11 09:31:59 +0530

    == implementation

---
+++ Strings/equals.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class equals {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "Hello";
+        String s3 = new String("Hello");
+        System.out.println(s1 == s2); // true -> Same memory address (Interning)
+        System.out.println(s1 == s3); // false -> Different memory address
+    }
+}
