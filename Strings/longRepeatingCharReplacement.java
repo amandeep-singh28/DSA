891f35ff5f0af17fefc5ed2e26d2520e8537b06c
Amandeep Singh <amandeep87557@gmail.com>
2026-03-17 12:42:49 +0530

    Longest Repeating Character Replacement (Optimized 2nd approach)

--- Strings/longRepeatingCharReplacement.java
+++ Strings/longRepeatingCharReplacement.java
@@ -1,5 +1,7 @@
 package Strings;
 
+import java.util.HashMap;
+
 public class longRepeatingCharReplacement {
     public int characterReplacement(String s, int k) {
         int maxLen = 0;
@@ -12,6 +14,37 @@ public class longRepeatingCharReplacement {
                 int changes = (j - i + 1) - maxFreq;
                 if (changes <= k) maxLen = Math.max(maxLen, j - i + 1);
                 else break;
+            }
+        }
+        return maxLen;
+    }
+    public int characterReplacement2(String s, int k) {
+        HashMap<Character, Integer> hm = new HashMap<>();
+        int right = 0, left = 0, maxLen = 0, maxFreq = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (hm.containsKey(s.charAt(i)) == false) {
+                hm.put(s.charAt(i), 1);
+                maxFreq = Math.max(maxFreq, 1);
+                int changes = (right - left + 1) - maxFreq;
+                if (changes <= k) maxLen = Math.max(maxLen, right - left + 1);
+                else {
+                    int freq1 = hm.get(s.charAt(left));
+                    hm.put(s.charAt(left), freq1 - 1);
+                    left++;
+                }
+                right++;
+            } else {
+                int freq = hm.get(s.charAt(i));
+                hm.put(s.charAt(i), freq + 1);
+                maxFreq = Math.max(maxFreq, freq + 1);
+                int changes = (right - left + 1) - maxFreq;
+                if (changes <= k) maxLen = Math.max(maxLen, right - left + 1);
+                else {
+                    int freq1 = hm.get(s.charAt(left));
+                    hm.put(s.charAt(left), freq1 - 1);
+                    left++;
+                }
+                right++;
             }
         }
         return maxLen;
