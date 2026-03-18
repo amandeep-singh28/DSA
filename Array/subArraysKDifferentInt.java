e8b776343633aa7d0ce2690d7e295db6a7897c6d
Amandeep Singh <amandeep87557@gmail.com>
2026-03-18 12:03:01 +0530

    SubArray with k different integer(Approach 2)

--- Array/subArraysKDifferentInt.java
+++ Array/subArraysKDifferentInt.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class subArraysKDifferentInt {
@@ -12,5 +13,33 @@ public class subArraysKDifferentInt {
             }
         }
         return count;
+    }
+    public int helper(int nums[], int goal) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        int left = 0, right = 0, count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (hm.containsKey(nums[i]) == false) {
+                hm.put(nums[i], 1);
+            } else {
+                int freq = hm.get(nums[i]);
+                hm.put(nums[i], freq + 1);
+            }
+            while (hm.size() > goal) {
+                int freq = hm.get(nums[left]);
+                hm.put(nums[left], freq - 1);
+                if (hm.get(nums[left]) == 0) {
+                    hm.remove(nums[left]);
+                }
+                left++;
+            }
+            count = count + (right - left + 1);
+            right++;
+        }
+        return count;
+    }
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        int count = helper(nums, k);
+        int count1 = helper(nums, k - 1);
+        return count - count1;
     }
 }
