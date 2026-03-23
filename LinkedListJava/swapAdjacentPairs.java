011ae3373260f1507756ef46caf79dd87b71e42a
Amandeep Singh <amandeep87557@gmail.com>
2026-03-23 19:09:25 +0530

    Swap Adjacent Pairs

---
+++ LinkedListJava/swapAdjacentPairs.java
@@ -0,0 +1,20 @@
+package LinkedListJava;
+
+public class swapAdjacentPairs {
+    public class ListNode {
+        ListNode next;
+        int val;
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode a = head;
+        ListNode b = head.next;
+        ListNode nextPair = b.next;
+        b.next = a;
+        a.next = swapPairs(nextPair);
+        return b;
+    }
+}
