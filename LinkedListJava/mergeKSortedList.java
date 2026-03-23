d75845d8370ea3fedbe0951d527dbf0bf8b3ae16
Amandeep Singh <amandeep87557@gmail.com>
2026-03-23 19:07:53 +0530

    Merge K Sorted List(Updated Divide & Conquer)

--- LinkedListJava/mergeKSortedList.java
+++ LinkedListJava/mergeKSortedList.java
@@ -35,16 +35,36 @@ public class mergeKSortedList {
         }
         return tempC.next;
     }
+    // public ListNode mergeKLists(ListNode[] lists) {
+    //     if (lists.length == 0) return null;
+    //     if (lists.length == 1) return lists[0];
+    //     ListNode mergeTwo = null;
+    //     for (int i = 0; i < lists.length - 1; i++) {
+    //         ListNode current = lists[i];
+    //         ListNode next = lists[i + 1];
+    //         if (i == 0) mergeTwo = merge2(current, next);
+    //         else mergeTwo = merge2(mergeTwo, next);
+    //     }
+    //     return mergeTwo;
+    // }
     public ListNode mergeKLists(ListNode[] lists) {
         if (lists.length == 0) return null;
-        if (lists.length == 1) return lists[0];
-        ListNode mergeTwo = null;
-        for (int i = 0; i < lists.length - 1; i++) {
-            ListNode current = lists[i];
-            ListNode next = lists[i + 1];
-            if (i == 0) mergeTwo = merge2(current, next);
-            else mergeTwo = merge2(mergeTwo, next);
+
+        ListNode ans = divide(lists, 0, lists.length - 1);
+        return ans;
+    }
+
+    private ListNode divide(ListNode[] lists, int left, int right) {
+        if (left == right) {
+            return lists[left];
         }
-        return mergeTwo;
+
+        int mid = left + (right - left) / 2;
+
+        ListNode l1 = divide(lists, left, mid);
+        ListNode l2 = divide(lists, mid + 1, right);
+
+        ListNode merged = merge2(l1, l2);
+        return merged;
     }
 }
