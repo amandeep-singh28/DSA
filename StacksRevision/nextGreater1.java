1fa92ab2ea79c07556f16c2e04919ee1a0549c31
Amandeep Singh <amandeep87557@gmail.com>
2026-03-31 13:01:13 +0530

    Next Greater Element 1 - Slightly Optimized O(n^2)

--- StacksRevision/nextGreater1.java
+++ StacksRevision/nextGreater1.java
@@ -1,25 +1,24 @@
 package StacksRevision;
+import java.util.Arrays;
 
 public class nextGreater1 {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int len1 = nums1.length;
         int len2 = nums2.length;
-        int[] ans = new int[len1];
-
+        int ans[] = new int[nums1.length];
+        Arrays.fill(ans, -1);
         for (int i = 0; i < len1; i++) {
             int idx = -1;
             for (int j = 0; j < len2; j++) {
                 if (nums1[i] == nums2[j]) {
                     idx = j;
+                    break;
                 }
             }
-            
-            for (int k = idx; k < len2; k++) {
-                if (nums2[k] > nums1[i]) {
-                    ans[i] = nums2[k];
+            for (int j = idx; j < len2; j++) {
+                if (nums2[j] > nums1[i]) {
+                    ans[i] = nums2[j];
                     break;
-                } else {
-                    ans[i] = -1;
                 }
             }
         }
