dafcf837ced9d0e19e0659d9a3a513d8fa407a32
Amandeep Singh <amandeep87557@gmail.com>
2026-04-02 10:03:34 +0530

    Partition List (O(1) without extra space)

--- LinkedListJava/partitionList.java
+++ LinkedListJava/partitionList.java
@@ -28,4 +28,28 @@ public class partitionList {
         temp1.next = greaterThanX.next;
         return lessThanX.next;
     }
+    public ListNode partition2(ListNode head, int x) {
+        ListNode lessThanX = new ListNode(-1);
+        ListNode  temp1 = lessThanX;
+        ListNode greaterThanX = new ListNode(-1);
+        ListNode  temp2 = greaterThanX;
+
+        ListNode temp = head;
+        while (temp != null) {
+            if (temp.val < x) {
+                temp1.next = temp;
+                temp1 = temp;
+                temp = temp.next;
+                temp1.next = null;
+            } else {
+                temp2.next = temp;
+                temp2 = temp;
+                temp = temp.next;
+                temp2.next = null;
+            }
+            
+        }
+        temp1.next = greaterThanX.next;
+        return lessThanX.next;
+    }
 }
