5f7fdd399761fba26f789eabf0dcd10c88354026
Amandeep Singh <amandeep87557@gmail.com>
2026-04-20 13:20:52 +0530

    2nd use of final keyword:- Using final along with methods

--- OOPs/useOfFinal.java
+++ OOPs/useOfFinal.java
@@ -1,10 +1,19 @@
 package OOPs;
 
+class ABC {
+    final void display() {
+        System.out.println("ABC Class");
+    }
+}
+
+class XYZ extends ABC {
+    // void display() {
+    //     System.out.println("XYZ Class");
+    // }
+}
 public class useOfFinal {
     public static void main(String[] args) {
-        final int a = 10;
-        System.out.println(a);
-        a = 20;
-        System.out.println(a);
+        XYZ obj1 = new XYZ();
+        obj1.display();
     }
 }
