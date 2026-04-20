94850a8507225ca7073e2749e9236e51a5497e46
Amandeep Singh <amandeep87557@gmail.com>
2026-04-20 13:10:25 +0530

    1st use of final keyword -> Using final with a variable

---
+++ OOPs/useOfFinal.java
@@ -0,0 +1,10 @@
+package OOPs;
+
+public class useOfFinal {
+    public static void main(String[] args) {
+        final int a = 10;
+        System.out.println(a);
+        a = 20;
+        System.out.println(a);
+    }
+}
