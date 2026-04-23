0a5f700a0176e6f587ebac7e6c6671c36059a1a9
Amandeep Singh <amandeep87557@gmail.com>
2026-04-23 11:42:41 +0530

    Longest Substring without repeating characters(Rev -> HashMap logic)

--- Practice/basic.java
+++ Practice/basic.java
@@ -1,24 +1,30 @@
 package Practice;
 
+import java.util.HashMap;
 
 public class basic {
-    public String reverseWords(String s) {
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0) return 0;
+        HashMap<Character, Integer> hm = new HashMap<>();
+        hm.put(s.charAt(0), 0);
+        int maxlen = 1;
         int length1 = s.length();
-
-        StringBuilder sb1 = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
-
-        for (int i = 0; i < length1 ; i++) {
+        int left = 0;
+        int right = 0;
+        for (int i = 1; i < length1; i++) {
             char ch = s.charAt(i);
-            if (ch != ' ') {
-                sb1.append(ch);
+            if (hm.containsKey(ch) == false) {
+                hm.put(ch, i);
+                right++;
+                maxlen = Math.max(maxlen, right - left + 1);
             } else {
-                sb2.append(sb1.reverse());
-                sb2.append(' ');
-                sb1.setLength(0);
+                int freq = hm.get(ch);
+                hm.put(ch, i);
+                right++;
+                left = Math.max(left, freq + 1);
+                maxlen = Math.max(maxlen, right - left + 1);
             }
         }
-        sb2.append(sb1.reverse());
-        return sb2.toString();
+        return maxlen;
     }
 }
