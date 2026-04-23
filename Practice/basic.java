c745bbc2afc51b65088ea09316468ea995dba40c
Amandeep Singh <amandeep87557@gmail.com>
2026-04-23 10:36:37 +0530

    To Lower Case(Rev)

--- Practice/basic.java
+++ Practice/basic.java
@@ -1,26 +1,17 @@
 package Practice;
 
-import java.util.Arrays;
 
 public class basic {
-    public boolean isAnagram(String s, String t) {
+    public String toLowerCase(String s) {
+        StringBuilder sb = new StringBuilder();
         int len1 = s.length();
-        int len2 = t.length();
-
-        int[] freq = new int[256];
         for (int i = 0; i < len1; i++) {
             char ch = s.charAt(i);
-            freq[ch - 'a']++;
+            if (ch >= 'A' && ch <= 'Z') {
+                int element = ch + 32;
+                sb.append((char)element);
+            } else sb.append(ch);
         }
-
-        for (int i = 0; i < len2; i++) {
-            char ch = t.charAt(i);
-            freq[ch - 'a']--;
-        }
-
-        for (int element : freq) {
-            if (element != 0) return false;
-        }
-        return true;
+        return sb.toString();
     }
 }
