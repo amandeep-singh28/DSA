feef2d0646b78fd2cba0fda2aaecd429a77d9f98
Amandeep Singh <amandeep87557@gmail.com>
2026-04-23 11:12:51 +0530

    Reverse words in a string part 3(Rev)

--- Practice/basic.java
+++ Practice/basic.java
@@ -3,21 +3,22 @@ package Practice;
 
 public class basic {
     public String reverseWords(String s) {
+        int length1 = s.length();
+
         StringBuilder sb1 = new StringBuilder();
         StringBuilder sb2 = new StringBuilder();
-        int length1 = s.length() - 1;
-        while (s.charAt(length1) == ' ' && length1 >= 0) length1--;
-        for (int i = length1; i >= 0; i--) {
+
+        for (int i = 0; i < length1 ; i++) {
             char ch = s.charAt(i);
             if (ch != ' ') {
                 sb1.append(ch);
-            } else if (ch == ' ' && s.charAt(i + 1) != ' '){
+            } else {
                 sb2.append(sb1.reverse());
                 sb2.append(' ');
                 sb1.setLength(0);
             }
         }
         sb2.append(sb1.reverse());
-        return sb2.toString().trim();
+        return sb2.toString();
     }
 }
