e48b0cd5f80346cc921ee04a0b614d9276538763
Amandeep Singh <amandeep87557@gmail.com>
2026-04-23 10:00:45 +0530

    Print all substring(Rev)

---
+++ Practice/basic.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class basic {
+    public static void main(String[] args) {
+        String s = "Hello";
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                System.out.println(s.substring(i, j + 1));
+            }
+        }
+    }
+}
