ca104389e3c9b2d8fcbd1eec26e50edb7be6813c
Amandeep Singh <amandeep87557@gmail.com>
2026-04-23 10:46:28 +0530

    Reverse String(Inplace Approach)

--- Practice/basic.java
+++ Practice/basic.java
@@ -2,15 +2,15 @@ package Practice;
 
 
 public class basic {
-    public boolean detectCapitalUse(String word) {
-        int count = 0;
-        int length = word.length();
-        for (int i = 0; i < length; i++) {
-            char ch = word.charAt(i);
-            if (ch >= 'A' && ch <= 'Z') count++;
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
         }
-        if (count == length || count == 0) return true;
-        if (count == 1 && (word.charAt(0) >= 'A' && word.charAt(0) <= 'Z')) return true;
-        return false;
     }
 }
